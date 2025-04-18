63396b8eff8d581e096c3ff62e4bd93014b31c77
Anaghadeshpande2004 <anaghadeshpande2004@gmail.com>
2025-04-18 22:45:36 +0530

    third commit

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.use("/api/orders", orderRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/token", userToken)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
